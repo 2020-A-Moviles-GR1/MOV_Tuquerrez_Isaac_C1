8027932945f5c9811c227ad4027453ef4c79e7b1
Isaac <wilianizac@gmail.com>
2020-06-18 08:32:27 -0500

    Hasta vidaActual ejemplo

--- src/MainKt.kt
+++ src/MainKt.kt
@@ -1,10 +1,16 @@
 import java.util.*
 import java.util.function.Consumer
+import kotlin.collections.ArrayList
 
 fun main(args: Array<String>) {
     print("Hola")
-    //mutables
+    // Ejemplo java:
+    // Int edad = 31;
+    // Mutables
     var edadProfesor = 31   // No especificamos el tipo de dato
+    // ; No es necesario
+    // Duck Typing
+    // var edadCachorro; X -> necesitamos el tipo de datos
     var edadCachorro: Int
     edadCachorro = 3
     edadProfesor = 32
@@ -36,8 +42,8 @@ fun main(args: Array<String>) {
     // calcularSueldo(1000.00, 14.00)
     calcularSueldo(1000.00, 14.00)
     calcularSueldo(
-            tasa = 16.00,
-            sueldo = 800.00
+        tasa = 16.00,
+        sueldo = 800.00
     ) // Named Parameters
     calcularSueldo(700.00)
     calcularSueldo(sueldo = 650.00)
@@ -51,29 +57,115 @@ fun main(args: Array<String>) {
     arregloCumpleanos.remove(30)
     print(arregloCumpleanos)
 
+
     arregloCumpleanos
-            .forEach {
-                println("Valor de la iteracion " + it)
-            }
+        .forEach { valorIteracion: Int ->
+            println("Valor iteracion: " + valorIteracion)
+        }
     arregloCumpleanos
-            .forEach { valorIteracion: Int ->
+        .forEach(
+            { valorIteracion: Int ->
                 println("Valor iteracion: " + valorIteracion)
             }
+        )
+
+    // Operadores -> TODOS LOS LENGUAJES
+    // ForEach no devuelve nada -> Unit
     arregloCumpleanos
-            .forEach(
-                    { valorIteracion: Int ->
-                        println("Valor iteracion: " + valorIteracion)
-                    }
-            )
+        .forEach { iteracion: Int ->
+            println("Valor de la iteracion " + iteracion)
+            println("Valor con -1 = ${iteracion * -1} ")
+        }
 
+    val respuestaArregloForEach = arregloCumpleanos
+        .forEachIndexed { index: Int, iteracion: Int ->
+            println("Valor de la iteracion " + iteracion)
+        }
+    println(respuestaArregloForEach) // Void Unit
 
+    // MAP -> Muta el arreglo (Cambia el arreglo)
+    // 1) Enviemos el nuevo valor de la iteracion
+    // 2) Nos devuelve es un NUEVO ARREGLO con los valores modificados
+    val respuestaMap = arregloCumpleanos
+        .map { iterador: Int ->
+            iterador * -1
+        }
+    val respuestaMapDos= arregloCumpleanos
+        .map { iterador: Int ->
+            val nuevoValor = iterador * -1
+            val otroValor = nuevoValor * 2
+            return@map Date()
+        }
+    println(respuestaMap)
+    println(respuestaMapDos)
+    println(arregloCumpleanos)
 
+    // Filter -> FILTRAR EL ARREGLO
+    // 1) Devolver una expresion (TRUE o FALSE)
+    // 2) Nuevo arreglo que cumpla esa expresion
+    val respuestaFilter = arregloCumpleanos
+        .filter {
+                iteracion:Int ->
+            val esMayorA23 = iteracion > 23
+            return@filter esMayorA23
+        }
+    arregloCumpleanos
+        .filter {
+                iteracion:Int -> iteracion > 23
+        }
+    println(respuestaFilter)
+    println(arregloCumpleanos)
+
+    //Any -> OR
+    //ALL -> AND
+    // no devuelve un arreglo
+    val respuestaAny:Boolean = arregloCumpleanos.any {
+        itera:Int ->
+        return@any itera > 25
+    }
+    print("\nRespuesta any ")
+    print(respuestaAny)
+
+    val respuestaAll:Boolean = arregloCumpleanos.all {
+        itera:Int ->
+        return@all itera < 65
+    }
+    print("\nrespuesta all ")
+    print(respuestaAll)
+
+    val respuestaReduce:Int = arregloCumpleanos.reduce{
+        acumulador,iteracion ->
+        return@reduce acumulador+iteracion
+    }
+
+    print("\nrespuesta reduce ")
+    print(respuestaReduce)
+
+    val respuestaFold:Int = arregloCumpleanos
+        .fold(
+            100,
+            { acumulador, iteracion ->
+                return@fold acumulador - iteracion
+            }
+        )
+
+    print("\nrespuesta reduce ")
+    print(respuestaReduce)
+
+    val vidaAcutal:Double = arregloCumpleanos.map{it * 0.8}
+        .filter {it > 18}
+        .fold(
+            100.00,{
+            acc, d -> acc-d}
+        )
+
+    println(vidaAcutal)
 }
 
 fun calcularSueldo(
-        sueldo: Double, // Requeridos!
-        tasa: Double = 12.00, // Tiene valor defecto
-        calculoEspecial: Int? = null // Pueden ser nulos
+    sueldo: Double, // Requeridos!
+    tasa: Double = 12.00, // Tiene valor defecto
+    calculoEspecial: Int? = null // Pueden ser nulos
 ): Double {
     if (calculoEspecial != null) {
         return sueldo * tasa * calculoEspecial
