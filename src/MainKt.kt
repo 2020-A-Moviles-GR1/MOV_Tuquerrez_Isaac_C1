14e0c931d155a9ec6a29a83cb61274e307f592e8
Isaac <wilianizac@gmail.com>
2020-06-18 07:15:02 -0500

    Project Override

--- src/MainKt.kt
+++ src/MainKt.kt
@@ -1,15 +1,19 @@
 import java.util.*
+import java.util.function.Consumer
 
 fun main(args: Array<String>) {
     print("Hola")
+    //mutables
     var edadProfesor = 31   // No especificamos el tipo de dato
     var edadCachorro: Int
     edadCachorro = 3
     edadProfesor = 32
     edadCachorro = 4
-
-
+    // Inmutables
     val numeroCuenta = 123456 // NO SE PUEDEN REASIGNAR
+    // numeroCuenta = 123
+
+    // Tipos variables
     val nombreProfesor: String = "Vicente Adrian"
     val sueldo: Double = 12.20
     val apellidosProfesor: Char = 'a'
@@ -28,7 +32,8 @@ fun main(args: Array<String>) {
     }
 
     val esSueldoMayorAlEstablecido = if (sueldo == 12.20) true else false
-
+    // EXPRESION ? X : Y
+    // calcularSueldo(1000.00, 14.00)
     calcularSueldo(1000.00, 14.00)
     calcularSueldo(
             tasa = 16.00,
@@ -36,9 +41,38 @@ fun main(args: Array<String>) {
     ) // Named Parameters
     calcularSueldo(700.00)
     calcularSueldo(sueldo = 650.00)
+
+    val arregloConstante: Array<Int> = arrayOf(1, 2, 3)
+    val arregloCumpleanos: ArrayList<Int> = arrayListOf(30, 31, 22, 23, 20)
+    print(arregloCumpleanos)
+    arregloCumpleanos.add(24)
+    print(arregloCumpleanos)
+    // arregloCumpleanos.remove(30)
+    arregloCumpleanos.remove(30)
+    print(arregloCumpleanos)
+
+    arregloCumpleanos
+            .forEach {
+                println("Valor de la iteracion " + it)
+            }
+    arregloCumpleanos
+            .forEach { valorIteracion: Int ->
+                println("Valor iteracion: " + valorIteracion)
+            }
+    arregloCumpleanos
+            .forEach(
+                    { valorIteracion: Int ->
+                        println("Valor iteracion: " + valorIteracion)
+                    }
+            )
+
+
+
 }
 
-fun calcularSueldo( sueldo: Double, tasa: Double = 12.00, // Tiene valor defecto
+fun calcularSueldo(
+        sueldo: Double, // Requeridos!
+        tasa: Double = 12.00, // Tiene valor defecto
         calculoEspecial: Int? = null // Pueden ser nulos
 ): Double {
     if (calculoEspecial != null) {
@@ -50,5 +84,5 @@ fun calcularSueldo( sueldo: Double, tasa: Double = 12.00, // Tiene valor defecto
 
 
 fun imprimirMensaje() { // Unit = Void
-    println("Hola Kotlin")
+    println("")
 }
